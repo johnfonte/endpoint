eacf97bdd3d4c8ec9d6aa0a7910889b8ba05165c
John Fonte <john@fonte.com>
2025-03-20 12:51:50 -0400

    initial

---
+++ src/Directory.java
@@ -0,0 +1,42 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Directory {
+    String name;
+    TreeMap<String, Directory> subdirectories;
+
+    public Directory(String name) {
+        this.name = name;
+        this.subdirectories = new TreeMap<>();
+    }
+
+    public void create(String directory, Directory existing) {
+        if (directory.isEmpty()) {
+            return;
+        }
+        if (subdirectories.containsKey(directory)) {
+            System.out.println("Directory already exists");
+        } else {
+            if (existing == null) {
+                subdirectories.put(directory, new Directory(directory));
+            } else {
+                subdirectories.put(directory, existing);
+            }
+        }
+    }
+
+    public void list(int depth) {
+        for (Map.Entry<String, Directory> entry : subdirectories.entrySet()) {
+            String spaces = "  ".repeat(depth);
+            System.out.println(spaces + entry.getKey());
+            entry.getValue().list(depth + 1);
+        }
+    }
+
+    public void delete(String directory) {
+        if (directory.isEmpty()) {
+            return;
+        }
+        subdirectories.remove(directory);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,143 @@
+/*
+## The problem
+
+ A common method of organizing files on a computer is to store them in hierarchical directories. For instance:
+
+ ```
+ photos/
+  birthdays/
+      joe/
+        mary/
+      vacations/
+    weddings/
+    ```
+
+ In this challenge, you will implement commands that allow a user to create, move and delete directories.
+
+ A successful solution will take the following input:
+
+```
+CREATE fruits
+CREATE vegetables
+CREATE grains
+CREATE fruits/apples
+CREATE fruits/apples/fuji
+LIST
+CREATE grains/squash
+MOVE grains/squash vegetables
+CREATE foods
+MOVE grains foods
+MOVE fruits foods
+MOVE vegetables foods
+LIST
+DELETE fruits/apples
+DELETE foods/fruits/apples
+LIST
+```
+
+ and produce the following output
+
+ ```
+ CREATE fruits
+CREATE vegetables
+CREATE grains
+CREATE fruits/apples
+CREATE fruits/apples/fuji
+LIST
+fruits
+  apples
+    fuji
+grains
+vegetables
+CREATE grains/squash
+MOVE grains/squash vegetables
+CREATE foods
+MOVE grains foods
+MOVE fruits foods
+MOVE vegetables foods
+LIST
+foods
+  fruits
+    apples
+      fuji
+  grains
+  vegetables
+    squash
+DELETE fruits/apples
+  Cannot delete fruits/apples - fruits does not exist
+DELETE foods/fruits/apples
+LIST
+foods
+  fruits
+  grains
+  vegetables
+    squash
+ */
+
+import java.util.Scanner;
+
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        runTestScenario();
+    }
+
+    public static void runTestScenario() {
+        final RootDirectory root = new RootDirectory();
+        root.create("fruits");
+        root.create("vegetables");
+        root.create("grains");
+        root.create("fruits/apples");
+        root.create("fruits/apples/fuji");
+        root.create("fruits/nuts/coconuts");
+        root.list();
+        System.out.println();
+
+        root.create("grains/squash");
+        root.move("grains/squash", "vegetables");
+        root.create("foods");
+        root.move("grains", "foods");
+        root.move("fruits", "foods");
+        root.move("vegetables", "foods");
+        root.move("blahblah", "foods");
+        root.move("foods", "blahblah");
+        root.list();
+        System.out.println();
+
+        root.delete("fffff");
+        root.delete("fruits/apples");
+        root.delete("foods/fruits/apples");
+        root.list();
+        System.out.println();
+
+    }
+
+    public static void runOnMachine() {
+        final RootDirectory root = new RootDirectory();
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("exit")) {
+                break;
+            }
+            String[] commands = command.split(" ");
+            switch (commands[0]) {
+                case "CREATE":
+                    root.create(commands[1]);
+                    break;
+                case "LIST":
+                    root.list();
+                    break;
+                case "DELETE":
+                    root.delete(commands[1]);
+                    break;
+                case "MOVE":
+                    root.move(commands[1], commands[2]);
+                    break;
+                default:
+                    System.out.println("Invalid command");
+            }
+        }
+    }
+}
